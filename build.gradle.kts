138b1d9f2de5cf5295a698decb20076adbd5e4bc
vladkinz <bqullk339@gmail.com>
2025-11-24 17:14:16 +0300

    сделал все как по уроку xd

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,13 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
 }
+buildscript {
+    repositories {
+        google()
+    }
+
+    dependencies {
+        val nav_version = "2.9.6"
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
+    }
+}
